cc5d202f50b6028c95c548f613adfc694499b1c3
localbobs <wlor0623@gmail.com>
2025-04-13 02:40:02 +0800

    2025年4月13日02:40:01

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
-const app = require('./index');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
+});
+
+// 导出为 Vercel 所需的 handler
 module.exports = app;
